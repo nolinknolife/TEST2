b1a2723dffc4b27daa85b8baa6090b7a57fce625
aaa <aaa@ggg.com>
2022-04-02 09:15:58 +0900

    commit test at master

--- test.py
+++ test.py
@@ -1,5 +1,16 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 for a in range(3):
     print(a)
-    
+
+sr = 320000
+mes_len_ms = 0.1
+mes_len_sa = int(sr * mes_len_ms/1000)
+freq_mhz = 250
+
+x_list = [a for a in range(mes_len_sa)]
+plt.plot(x_list, np.sin(x_list), marker="o")
+plt.show()
+
+
